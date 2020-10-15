ea77bf280a53bb1f0eb0169d5664b40005e60380
johanuribe03 <johan.uribe@gmail.com>
2020-10-15 23:49:35 +0000

    mainUpdate2

--- src/js/views/landing.js
+++ src/js/views/landing.js
@@ -58,32 +58,7 @@ export const Landing = () => {
 					</Nav.Link>
 				</Nav>
 			</div>
-			{/* <section className="colored-section" id="testimonials">
-				<div id="testimonial-carousel" className="carousel slide" data-ride="false">
-					<div className="carousel-inner">
-						<div className="carousel-item active container-fluid">
-							<h2 className="testimonial-text">
-								I no longer have to sniff other dogs for love. Ive found the hottest Corgi on TinDog.
-								Woof.
-							</h2>
-							<em>Pebbles, New York</em>
-						</div>
-						<div className="carousel-item container-fluid">
-							<h2 className="testimonial-text">
-								My dog used to be so lonely, but with TinDogs help, theyve found the love of their life.
-								I think.
-							</h2>
-							<em>Beverly, Illinois</em>
-						</div>
-					</div>
-					<a className="carousel-control-prev" href="#testimonial-carousel" role="button" data-slide="prev">
-						<span className="carousel-control-prev-icon" />
-					</a>
-					<a className="carousel-control-next" href="#testimonial-carousel" role="button" data-slide="next">
-						<span className="carousel-control-next-icon" />
-					</a>
-				</div>
-			</section> */}
+			
 			<Carousel>
 				<Carousel.Item className="slide">
 					<div className="img-container">
